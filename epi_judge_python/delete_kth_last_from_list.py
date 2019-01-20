1fadc19954bc3ddadc2ab280688c427d0f93d058
Vijay Velagapudi <acchi369@gmail.com>
2019-01-20 14:18:49 -0800

    Add solution for delete kth node for linked list

--- epi_judge_python/delete_kth_last_from_list.py
+++ epi_judge_python/delete_kth_last_from_list.py
@@ -3,8 +3,24 @@ from test_framework import generic_test
 
 # Assumes L has at least k nodes, deletes the k-th last node in L.
 def remove_kth_last(L, k):
-    # TODO - you fill in here.
-    return None
+    prev = None
+    curr, runner = L, L
+
+    for _ in range(k):
+        if runner:
+            runner = runner.next
+        else:
+            raise ValueError('invalid input provided to function')
+
+    while runner:
+        prev, curr, runner = curr, curr.next, runner.next
+
+    if prev:
+        prev.next = curr.next
+    else:
+        L = curr.next
+
+    return L
 
 
 if __name__ == '__main__':
