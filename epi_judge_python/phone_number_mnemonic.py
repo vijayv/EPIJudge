7981de6ca6e10c7c03e78e665193e716caa02d8a
Vijay Velagapudi <acchi369@gmail.com>
2019-01-20 14:50:02 -0800

    Add solution for phone num mnemonic

--- epi_judge_python/phone_number_mnemonic.py
+++ epi_judge_python/phone_number_mnemonic.py
@@ -1,9 +1,32 @@
 from test_framework import generic_test, test_utils
 
-
 def phone_mnemonic(phone_number):
-    # TODO - you fill in here.
-    return []
+
+    mapping = {
+        '0': ['0'],
+        '1': ['1'],
+        '2': ['A', 'B', 'C'],
+        '3': ['D', 'E', 'F'],
+        '4': ['G', 'H', 'I'],
+        '5': ['J', 'K', 'L'],
+        '6': ['M', 'N', 'O'],
+        '7': ['P', 'Q', 'R', 'S'],
+        '8': ['T', 'U', 'V'],
+        '9': ['W', 'X', 'Y', 'Z'],
+    }
+
+    def helper(digits, so_far="", output=[]):
+        if len(digits) == 0:
+            output.append(so_far)
+            return
+        remaining = "" if len(digits) == 1 else digits[1:]
+        for c in mapping.get(digits[0]):
+            helper(remaining, so_far + c, output)
+
+
+    output = []
+    helper(phone_number, "", output)
+    return output
 
 
 if __name__ == '__main__':
