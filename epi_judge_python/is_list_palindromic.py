c4632d6f90cde0c77dcc7af1d1496a4dac077568
Vijay Velagapudi <acchi369@gmail.com>
2019-01-20 19:09:16 -0800

    Add solution for is linked list a palindrome

--- epi_judge_python/is_list_palindromic.py
+++ epi_judge_python/is_list_palindromic.py
@@ -1,8 +1,37 @@
 from test_framework import generic_test
 
 
+# We only have to reverse the second half of the list because we only need to
+# check half to see if it matches with the other half.
 def is_linked_list_a_palindrome(L):
-    # TODO - you fill in here.
+
+    def reverse(head):
+        prev, curr = None, head
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev, curr = curr, temp
+
+        return prev
+
+    if not L:
+        return True
+
+    slow = L
+    runner = L
+    while runner and runner.next:
+        slow = slow.next
+        runner = runner.next.next
+
+    Lr = reverse(slow)
+
+    curr = L
+    curr_r = Lr
+    while curr_r:
+        if curr.data != curr_r.data:
+            return False
+        curr, curr_r = curr.next, curr_r.next
+
     return True
 
 
