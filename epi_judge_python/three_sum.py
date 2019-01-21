ca2f4dd79d30c96a8c8a6abe50d90b6d95b8828b
Vijay Velagapudi <acchi369@gmail.com>
2019-01-20 18:20:38 -0800

    Add solution for three sum

--- epi_judge_python/three_sum.py
+++ epi_judge_python/three_sum.py
@@ -1,9 +1,21 @@
 from test_framework import generic_test
 
 
+# There is a more optimal solution than this that allows for O(n) rather than
+# O(n^2). However, it requires non-trivial intuition. We can also probably
+# improve this solution to use no space by using binary search instead of a set.
+
 def has_three_sum(A, t):
-    # TODO - you fill in here.
-    return True
+    A.sort()
+    vals = set(A)
+
+    for c1 in A:
+        for c2 in A:
+            temp = t - (c1 + c2)
+            if temp in vals:
+                return True
+
+    return False
 
 
 if __name__ == '__main__':
