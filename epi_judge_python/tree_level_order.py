b9e40298224e6a25b1eb939c528dc0de893ed00e
Vijay Velagapudi <acchi369@gmail.com>
2019-01-20 18:32:34 -0800

    Add solution for level order traversal

--- epi_judge_python/tree_level_order.py
+++ epi_judge_python/tree_level_order.py
@@ -1,9 +1,25 @@
 from test_framework import generic_test
-
+from collections import deque
 
 def binary_tree_depth_order(tree):
-    # TODO - you fill in here.
-    return []
+    if not tree:
+        return []
+    output = []
+    queue = [(tree, 0)]
+
+    while queue:
+        curr, depth = queue.pop()
+        try:
+            output[depth].appendleft(curr.data)
+        except IndexError:
+            output.append(deque([curr.data]))
+
+        if curr.left:
+            queue.append((curr.left, depth+1))
+        if curr.right:
+            queue.append((curr.right, depth+1))
+
+    return [list(c) for c in output]
 
 
 if __name__ == '__main__':
