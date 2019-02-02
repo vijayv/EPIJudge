9e9c0c4c511a4b31e107e6c63b6d477162ef3545
Vijay Velagapudi <acchi369@gmail.com>
2019-02-02 11:45:39 -0800

    Add solution for generate power set

--- epi_judge_python/power_set.py
+++ epi_judge_python/power_set.py
@@ -2,11 +2,24 @@ from test_framework import generic_test, test_utils
 
 
 def generate_power_set(S):
-    # TODO - you fill in here.
-    return []
+
+    output = [[]]
+
+    def helper(S, so_far=[], output=[]):
+        if not len(S):
+            output.append(so_far)
+        else:
+            helper(S[1:], so_far + [S[0]], output)
+            helper(S[1:], so_far, output)
+
+    for idx, num in enumerate(S):
+        helper(S[idx+1:], [num], output)
+
+    return output
 
 
 if __name__ == '__main__':
+    print(generate_power_set([0]))
     exit(
         generic_test.generic_test_main("power_set.py", 'power_set.tsv',
                                        generate_power_set,
